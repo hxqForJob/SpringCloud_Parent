a47e6d02174b6142b652e9e5d67987d54b3349dd
qiangzai <hxq_job@sina.com>
2020-07-13 14:50:22 +0800

    使用Hystrix服务组件消费者6060

---
+++ Cloud_ConsumerFeignHystrix_Order6060/src/main/java/com/hxq/springcloud/OrderOpenFeignHystrix6060Main.java
@@ -0,0 +1,11 @@
+package com.hxq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderOpenFeignHystrix6060Main {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderOpenFeignHystrix6060Main.class,args);
+    }
+}
