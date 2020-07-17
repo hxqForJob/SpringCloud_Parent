833cfa1a48e5cbe73b95ee62a37167a8ea967271
qiangzai <hxq_job@sina.com>
2020-07-17 20:17:55 +0800

    服务提供者Hystirx服务降级

--- Cloud_Provider_Hystrix_Payment8007/src/main/java/com/hxq/springcloud/ProviderHystixMain8007.java
+++ Cloud_Provider_Hystrix_Payment8007/src/main/java/com/hxq/springcloud/ProviderHystixMain8007.java
@@ -2,10 +2,14 @@ package com.hxq.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrix
 public class ProviderHystixMain8007 {
     public static void main(String[] args) {
         SpringApplication.run(ProviderHystixMain8007.class,args);
