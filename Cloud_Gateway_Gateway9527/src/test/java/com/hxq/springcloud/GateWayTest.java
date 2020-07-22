8b15644d2cb44b6968b9d9e66b626fba62ce9a4b
qiangzai <hxq_job@sina.com>
2020-07-22 16:38:10 +0800

    测试获取本地时间

---
+++ Cloud_Gateway_Gateway9527/src/test/java/com/hxq/springcloud/GateWayTest.java
@@ -0,0 +1,10 @@
+package com.hxq.springcloud;
+
+import java.time.ZonedDateTime;
+
+public class GateWayTest {
+    public static void main(String[] args) {
+        ZonedDateTime dateTime=ZonedDateTime.now();
+        System.out.println(dateTime);
+    }
+}
