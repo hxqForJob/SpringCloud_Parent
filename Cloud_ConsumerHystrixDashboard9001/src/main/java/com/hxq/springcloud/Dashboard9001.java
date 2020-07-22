d7347840cdc2f2b0f2865e835cbc8cf0efab5a13
qiangzai <hxq_job@sina.com>
2020-07-22 10:21:17 +0800

    添加服务监控Dashboard监控

---
+++ Cloud_ConsumerHystrixDashboard9001/src/main/java/com/hxq/springcloud/Dashboard9001.java
@@ -0,0 +1,13 @@
+package com.hxq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class Dashboard9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Dashboard9001.class,args);
+    }
+}
