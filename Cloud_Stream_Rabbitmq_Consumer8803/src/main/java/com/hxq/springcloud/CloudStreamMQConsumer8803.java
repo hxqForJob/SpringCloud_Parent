87e740a1244d63994b107482b666a562825d710f
qiangzai <hxq_job@sina.com>
2020-07-23 19:00:09 +0800

    SpringCloud消息驱动，消息消费者8803

---
+++ Cloud_Stream_Rabbitmq_Consumer8803/src/main/java/com/hxq/springcloud/CloudStreamMQConsumer8803.java
@@ -0,0 +1,11 @@
+package com.hxq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamMQConsumer8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamMQConsumer8803.class,args);
+    }
+}
