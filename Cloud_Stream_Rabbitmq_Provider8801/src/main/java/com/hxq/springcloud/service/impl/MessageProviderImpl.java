68388768da83124e7b9e1cff514ac3f2f33e0840
qiangzai <hxq_job@sina.com>
2020-07-23 19:00:38 +0800

    添加扫描注解

--- Cloud_Stream_Rabbitmq_Provider8801/src/main/java/com/hxq/springcloud/service/impl/MessageProviderImpl.java
+++ Cloud_Stream_Rabbitmq_Provider8801/src/main/java/com/hxq/springcloud/service/impl/MessageProviderImpl.java
@@ -5,11 +5,13 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.UUID;
 
 @EnableBinding(Source.class) //定义消息的推送管道
+@Component
 public class MessageProviderImpl implements IMesaageProvider {
     @Resource
     private MessageChannel output; // 消息发送管道
