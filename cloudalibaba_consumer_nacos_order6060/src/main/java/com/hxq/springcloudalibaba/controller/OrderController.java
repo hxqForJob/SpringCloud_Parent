bf800183026a25b7c489548f624efeaa7635e977
qiangzai <hxq_job@sina.com>
2020-07-24 18:05:59 +0800

    添加springcloudalibaba_consumer_order6060模块

---
+++ cloudalibaba_consumer_nacos_order6060/src/main/java/com/hxq/springcloudalibaba/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.hxq.springcloudalibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.nacos-payment-service}")
+    private String serverURL;
+
+    @GetMapping(value = "/consumer/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Long id)
+    {
+        return restTemplate.getForObject(serverURL+"/payment/nacos/"+id,String.class);
+    }
+
+}
