12305c967240573827f6bb7825a070d0de38e585
qiangzai <hxq_job@sina.com>
2020-07-27 16:00:27 +0800

    添加CloudAlibaba Nacos配置中心客户端模块

---
+++ cloudalibaba_config_nacos_client3377/src/main/java/com/hxq/springcloudalibaba/CloudAlibabaConfigClient3377Main.java
@@ -0,0 +1,13 @@
+package com.hxq.springcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CloudAlibabaConfigClient3377Main {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAlibabaConfigClient3377Main.class,args);
+    }
+}
